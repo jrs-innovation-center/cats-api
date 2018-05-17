efd2032a64f86a2b2935a612aa0eb30b6ed9a876
tripott <tripott@gmail.com>
2018-05-17 10:23:14 -0400

    removing tests

--- test/0--nuke-testz.js
+++
@@ -1,9 +0,0 @@
-const test = require('tape')
-const dal = require('../dal.js')
-
-test('Nuke Test Database', t => {
-  dal.nukeDocs((err, data) => {
-    err ? console.log('the err', err) : console.log('the data', data)
-    t.end()
-  })
-})
