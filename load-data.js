502506e2aca2d435424c5c3d265b6fd881fe9885
shoraka-blair <suzanne.blair@powerhousetech.co>
2018-05-17 10:10:46 -0400

    changes load-data

--- load-data.js
+++ load-data.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 
 const PouchDB = require('pouchdb')
-const db = new PouchDB(process.env.COUCHDB_URL + process.env.COUCHDB_NAME)
+const db = new PouchDB(process.env.COUCHDB_URL)
 
 db
   .bulkDocs([
